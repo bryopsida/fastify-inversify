9da50b857c3712e5c7f0b0bbdbe6ea52fe724d43
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-02-12 17:21:00 -0600

    :test_tube: Add test stubs

---
+++ test/fastify.inversify.plugin.spec.ts
@@ -0,0 +1,10 @@
+import {describe, it, expect } from "@jest/globals"
+
+describe('Inversify Plugin', () => {
+  it('should provide container to requests', () => {
+    expect(false).toBeTruthy()
+  })
+  it('should clean up request scoped container when response is sent', () => {
+    expect(false).toBeTruthy()
+  })
+})
